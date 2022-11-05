ca55becc013c7341ab437890569d20b3ffcbd1a2
Hlalela29 <113600201+Hlalela29@users.noreply.github.com>
2022-11-05 11:56:48 +0200

    Add files via upload

---
+++ Forest_Game/Assets/Scripts/mainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour {
+
+	public string loadLevel;
+
+	public void playGame()
+	{
+		SceneManager.LoadScene (loadLevel);
+	}
+	public void quitGame()
+	{
+		Application.Quit();
+	}
+
+}
---
+++ Forest_Game/Assets/Scripts/playCollision.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class playCollision : MonoBehaviour {
+
+	public int score = 0;
+	public Text DisplayScore;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DisplayScore.GetComponent<Text>().text = "Score: " + score.ToString ();
+	}
+
+	public void OnTriggerEnter(Collider col)
+	{
+		if(col.gameObject.name == "TreasureBox")
+		{
+		  score = score + 5;
+		  Destroy(col.gameObject);
+		}
+		if(col.gameObject.name == "TreasureBox (1)")
+		{
+			score = score + 5;
+			Destroy(col.gameObject);
+		}
+		if(col.gameObject.name == "TreasureBox (2)")
+		{
+			score = score + 5;
+			Destroy(col.gameObject);
+		}
+		if(col.gameObject.name == "TreasureBox (3)")
+		{
+			score = score + 5;
+			Destroy(col.gameObject);
+		}
+		if(col.gameObject.name == "TreasureBox (4)")
+		{
+			score = score + 5;
+			Destroy(col.gameObject);
+		}
+
+		if (score >= 25)
+		{
+			SceneManager.LoadScene("Congratulations");
+
+		}
+
+	}
+}
